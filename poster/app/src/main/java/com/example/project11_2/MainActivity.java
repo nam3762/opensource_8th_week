388129308865025989b09a731213febca8e918dc
nam3762 <nam37623762@gmail.com>
2023-04-30 21:21:13 +0900

    2023-04-30 21:21

---
+++ poster/app/src/main/java/com/example/project11_2/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.project11_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+@SuppressWarnings("deprecation")
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("갤러리 영화 포스터(개선)");
+
+        Gallery gallery = (Gallery) findViewById(R.id.gallery1);
+
+        MyGalleryAdapter galAdapter = new MyGalleryAdapter(this);
+        gallery.setAdapter(galAdapter);
+    }
+
+    public class MyGalleryAdapter extends BaseAdapter {
+
+        Context context;
+        Integer[] posterID = { R.drawable.mov11, R.drawable.mov12,
+                R.drawable.mov13, R.drawable.mov14, R.drawable.mov15,
+                R.drawable.mov16, R.drawable.mov17, R.drawable.mov18,
+                R.drawable.mov19, R.drawable.mov20 };
+
+        String[] posterTitle = { "레옹", "주먹왕 랄프2", "타짜","걸캅스", "도굴", "어벤저스",
+                "엑시트", "캡틴 마블", "봉오동 전투", "분노의 질주"};
+
+        public MyGalleryAdapter(Context c) {
+            context = c;
+        }
+
+        public int getCount() {
+            // TODO Auto-generated method stub
+            return posterID.length;
+        }
+
+        public Object getItem(int arg0) {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        public long getItemId(int position) {
+            // TODO Auto-generated method stub
+            return 0;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageview = new ImageView(context);
+            imageview.setLayoutParams(new Gallery.LayoutParams(200, 300));
+            imageview.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            imageview.setPadding(5, 5, 5, 5);
+            imageview.setImageResource(posterID[position]);
+
+            final int pos = position;
+            imageview.setOnTouchListener(new View.OnTouchListener() {
+                public boolean onTouch(View v, MotionEvent event) {
+                    ImageView ivPoster = (ImageView) findViewById(R.id.ivPoster);
+                    ivPoster.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                    ivPoster.setImageResource(posterID[pos]);
+
+
+
+                    return false;
+                }
+            });
+
+            return imageview;
+        }
+    }
+
+}
